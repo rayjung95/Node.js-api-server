0c62c43e2c404cca9540fd3248889d4f5bf1a4d7
LanceM26 <lancemagnaye@gmail.com>
2019-02-11 14:23:24 -0800

    added MVC + HBS

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var employeesRouter = require('./routes/employees');
+var customersRouter = require('./routes/customers');
+var servicessRouter = require('./routes/services');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/employees', employeesRouter);
+app.use('/customers', customersRouter);
+app.use('/services', servicessRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken')
+var mysql = require('mysql')
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "a01026675-test-db.cf9zyau6meiw.us-west-2.rds.amazonaws.com",
+  user: "node",
+  password: "password",
+  database: "unplug_and_thrive"
+})
+
+function getConnection() {
+  return pool
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const queryString = "SELECT * FROM employee WHERE email = '?' AND password = '?"
+  var successJSON
+  var user
+
+  getConnection().query(queryString, [email, password], (err, rows, fields) => {
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      res.end()
+      return
+    }
+
+    if (rows.length == 0) {
+      res.send({ status: "error" })
+    }
+
+    user = rows[0];
+    successJSON = { status: "success", account: rows[0] };
+  })
+  jwt.sign({user}, 'secret', (err, token) => {
+    res.json({
+      token,
+    })
+
+  })
+  // res.json(successJSON)
 });
 
 module.exports = router;
