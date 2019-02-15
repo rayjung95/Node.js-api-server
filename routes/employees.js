7718af1d436f4c8a1e242ba4ef325fa3b5d263ac
LanceM26 <lancemagnaye@gmail.com>
2019-02-14 23:47:52 -0800

    Refactored endpoints as well as maintain code cleanliness with StandardJS

--- routes/employees.js
+++ routes/employees.js
@@ -1,122 +1,114 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var generator = require('generate-password')
-var db = require('../db/db');
+var db = require('../db/db')
 var jwt = require('jsonwebtoken')
 
-function verifyToken(req, res, next) {
-    // Get auth header value
-    const bearerHeader = req.headers['authorization'];
-    // Check if bearer is undefined
-    if (typeof bearerHeader !== 'undefined') {
-        // Split at the space
-        const bearer = bearerHeader.split(' ');
-        // Get token from array
-        const bearerToken = bearer[1];
-        // Set the token
-        req.token = bearerToken;
-        // Next middleware
-        next();
-    } else {
-        // Forbidden
-        res.sendStatus(403);
-    }
+function verifyToken (req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization']
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ')
+    // Get token from array
+    const bearerToken = bearer[1]
+    // Set the token
+    req.token = bearerToken
+    // Next middleware
+    next()
+  } else {
+    // Forbidden
+    res.sendStatus(403)
+  }
 }
 
-router.get("/", verifyToken, (req, res) => {
-
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
+router.get('/', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403)
+    } else {
+      const queryString = 'SELECT * FROM employee'
+      db.query(queryString, (err, rows, fields) => {
         if (err) {
-            res.sendStatus(403);
-        } else {
-            const queryString = "SELECT * FROM employee"
-            db.query(queryString, (err, rows, fields) => {
-                if (err) {
-                    console.log("Failed to query for employees: " + err)
-                    res.sendStatus(500)
-                    res.end()
-                    return
-                }
-
-                console.log("I think we fetched it")
-                res.json(rows)
-                return
-            })
+          console.log('Failed to query for employees: ' + err)
+          res.sendStatus(500)
+          res.end()
+          return
         }
-    });
+
+        console.log('I think we fetched it')
+        res.json(rows)
+      })
+    }
+  })
 })
 
+router.get('/:id', (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403)
+    } else {
+      console.log('Fetching user with id: ' + req.params.id)
 
-router.get("/:id", (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
+      const userID = req.params.id
+      const queryString = 'SELECT * FROM employee WHERE employee_id = ?'
+      db.query(queryString, [userID], (err, rows, fields) => {
         if (err) {
-            res.sendStatus(403);
-        } else {
-            console.log("Fetching user with id: " + req.params.id)
-
-            const userID = req.params.id
-            const queryString = "SELECT * FROM employee WHERE employee_id = ?"
-            db.query(queryString, [userID], (err, rows, fields) => {
-
-                if (err) {
-                    console.log("Failed to query for users: " + err)
-                    res.sendStatus(500)
-                    res.end()
-                    return
-                }
-
-                // if (rows.length != 1) {
-                //   res.json({error: "User with ID: " + userID + " not found."})
-                //   res.end()
-                // }
-
-                console.log("I think we fetched it")
-                console.log(rows);
-                res.json(rows)
-                return
-            })
+          console.log('Failed to query for users: ' + err)
+          res.sendStatus(500)
+          res.end()
+          return
         }
-    });
-    // res.end()
+
+        // if (rows.length != 1) {
+        //   res.json({error: "User with ID: " + userID + " not found."})
+        //   res.end()
+        // }
+
+        console.log('I think we fetched it')
+        console.log(rows)
+        res.json(rows)
+      })
+    }
+  })
+  // res.end()
 })
 
 router.post('/create', (req, res) => {
-    jwt.verify(req.token, 'secretkey', (err, authData) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403)
+    } else {
+      console.log('Trying to create a new user')
+
+      const firstName = req.body.first_name
+      const lastName = req.body.last_name
+      const address = req.body.address
+      const zipCode = req.body.zip_code
+      const phoneNumber = req.body.phone_number.replace(/-/g, '')
+      const email = req.body.email
+
+      const userName = (firstName[0] + lastName).toLowerCase()
+      const password = generator.generate({
+        length: 10,
+        numbers: true
+      })
+
+      const queryString = 'INSERT INTO employee (first_name, last_name, user_name, password, address, zip, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+
+      db.query(queryString, [firstName, lastName, userName, password, address, zipCode, phoneNumber, email], (err, results, field) => {
         if (err) {
-            res.sendStatus(403);
-        } else {
-            console.log("Trying to create a new user")
-
-            const first_name = req.body.first_name
-            const last_name = req.body.last_name
-            const address = req.body.address
-            const zip_code = req.body.zip_code
-            const phone_number = req.body.phone_number.replace(/-/g, "")
-            const email = req.body.email
-
-            const user_name = (first_name[0] + last_name).toLowerCase()
-            const password = generator.generate({
-                length: 10,
-                numbers: true
-            });
-
-            const queryString = "INSERT INTO employee (first_name, last_name, user_name, password, address, zip, phone, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-
-            db.query(queryString, [first_name, last_name, user_name, password, address, zip_code, phone_number, email], (err, results, field) => {
-                if (err) {
-                    console.log("Failed to insert new user: " + err)
-                    res.sendStatus(500)
-                }
-
-                console.log("Inserted a new user with id: ", results.insertId)
-                res.end()
-                return
-            })
-            res.end();
-            return
+          console.log('Failed to insert new user: ' + err)
+          res.sendStatus(500)
         }
-    });
 
+        console.log('Inserted a new user with id: ', results.insertId)
+        res.end()
+      })
+      res.end()
+    }
+  })
 })
 
 module.exports = router
--- routes/services.js
+++ routes/services.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 // var mysql = require('mysql')
-var db = require('../db/db');
+var db = require('../db/db')
 
 // const pool = mysql.createPool({
 //   connectionLimit: 10,
@@ -15,64 +15,62 @@ var db = require('../db/db');
 //   return pool
 // }
 
-router.get("/", (req, res) => {
-  const queryString = "SELECT * FROM service"
+router.get('/', (req, res) => {
+  const queryString = 'SELECT * FROM service'
   db.query(queryString, (err, rows, fields) => {
-      if (err) {
-          console.log("Failed to query for services: " + err)
-          res.sendStatus(500)
-          res.end()
-          return
-      }
-      res.json(rows)
+    if (err) {
+      console.log('Failed to query for services: ' + err)
+      res.sendStatus(500)
+      res.end()
+      return
+    }
+    res.json(rows)
   })
 })
 
-router.get("/:id", (req, res) => {
-  console.log("Fetching user with id: " + req.params.id)
+router.get('/:id', (req, res) => {
+  console.log('Fetching user with id: ' + req.params.id)
 
   const userID = req.params.id
-  const queryString = "SELECT * FROM service WHERE service_id = ?"
+  const queryString = 'SELECT * FROM service WHERE service_id = ?'
   db.query(queryString, [userID], (err, rows, fields) => {
-    
-      if (err) {
-          console.log("Failed to query for customers: " + err)
-          res.sendStatus(500)
-          res.end()
-          return
-      }
+    if (err) {
+      console.log('Failed to query for customers: ' + err)
+      res.sendStatus(500)
+      res.end()
+      return
+    }
 
-      // if (rows.length != 1) {
-      //   res.json({error: "User with ID: " + userID + " not found."})
-      //   res.end()
-      // }
+    // if (rows.length != 1) {
+    //   res.json({error: "User with ID: " + userID + " not found."})
+    //   res.end()
+    // }
 
-      console.log("I think we fetched it")
-      res.json(rows)
+    console.log('I think we fetched it')
+    res.json(rows)
   })
 
   // res.end()
 })
 
 router.post('/create', (req, res) => {
-
   const name = req.body.name
   const provider = req.body.provider
   const description = req.body.description
-  const manual_link = req.body.manual_link
+  const manualLink = req.body.manual_link
 
-  const queryString = "INSERT INTO service (name, provider, description, manual_link) VALUES (?, ?, ?, ?)"
-  db.query(queryString, [first_name, last_name, user_name, password, address, zip_code, phone_number, email], (err, results, field) => {
-      if (err) {
-          console.log("Failed to insert new service: " + err)
-          res.sendStatus(500)
-      }
+  const queryString = 'INSERT INTO service (name, provider, description, manual_link) VALUES (?, ?, ?, ?)'
+  db.query(queryString, [name, provider, description, manualLink], (err, results, field) => {
+    if (err) {
+      console.log('Failed to insert new service: ' + err)
+      res.sendStatus(500)
+    }
 
-      console.log("Inserted a new user with service: ", results.insertId)
-      res.end()
+    console.log('Inserted a new user with service: ', results.insertId)
+    res.end()
   })
 
-  res.end();
+  res.end()
 })
 
 module.exports = router
