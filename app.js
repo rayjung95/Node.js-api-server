75b5decb0fb371070c3269f404f47e20ffbf8f28
rayjung95 <rayjung95@gmail.com>
2019-02-15 08:28:31 -0800

    add ACAO

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type,  Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT,  POST, PATCH, DELETE, GET');
+      return res.status(200).json({})
+  }
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/employees', employeesRouter);
 app.use('/customers', customersRouter);
